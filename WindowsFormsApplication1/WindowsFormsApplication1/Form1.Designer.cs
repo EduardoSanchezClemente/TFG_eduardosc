88f8e1da6abda6ea17ec26c9cf91853e6357015f
Eduardo <edusanchez1993@gmail.com>
2017-07-04 14:38:30 +0200

    Código del Visual Studio

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.start_button = new System.Windows.Forms.Button();
+            this.text_serial_port = new System.Windows.Forms.TextBox();
+            this.trace_box = new System.Windows.Forms.TextBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // start_button
+            // 
+            this.start_button.Location = new System.Drawing.Point(27, 12);
+            this.start_button.Name = "start_button";
+            this.start_button.Size = new System.Drawing.Size(75, 23);
+            this.start_button.TabIndex = 0;
+            this.start_button.Text = "START";
+            this.start_button.UseVisualStyleBackColor = true;
+            this.start_button.Click += new System.EventHandler(this.start_button_Click);
+            // 
+            // text_serial_port
+            // 
+            this.text_serial_port.Location = new System.Drawing.Point(165, 15);
+            this.text_serial_port.Name = "text_serial_port";
+            this.text_serial_port.Size = new System.Drawing.Size(75, 20);
+            this.text_serial_port.TabIndex = 1;
+            this.text_serial_port.Text = "COM3";
+            // 
+            // trace_box
+            // 
+            this.trace_box.Location = new System.Drawing.Point(27, 60);
+            this.trace_box.Multiline = true;
+            this.trace_box.Name = "trace_box";
+            this.trace_box.ReadOnly = true;
+            this.trace_box.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.trace_box.Size = new System.Drawing.Size(930, 200);
+            this.trace_box.TabIndex = 2;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(969, 372);
+            this.Controls.Add(this.trace_box);
+            this.Controls.Add(this.text_serial_port);
+            this.Controls.Add(this.start_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start_button;
+        private System.Windows.Forms.TextBox text_serial_port;
+        private System.Windows.Forms.TextBox trace_box;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+using System.IO;
+
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        int var_rssi = -100;
+        UInt16 var_node = 0;  //Como lo inicializo?
+        UInt16 var_placa = 0;  //Como lo inicializo?
+        UInt16 aux_nodo = 0;  //Como lo inicializo?
+        UInt16 aux_placa = 0;
+        int aux = 0;
+        int aux2 = 0;
+        int posicion_nodo = 0;
+        int posicion_placa = 0;
+        int [,] placa_nod = new int[10,10] { { -100, -100, -100, -100, -100, -100, -100, -100, -100, -100 }, { -100, -100, -100, -100, -100, -100, -100, -100, -100, -100 }, { -100, -100, -100, -100, -100, -100, -100, -100, -100, -100 }, { -100, -100, -100, -100, -100, -100, -100, -100, -100, -100 }, { -100, -100, -100, -100, -100, -100, -100, -100, -100, -100 }, { -100, -100, -100, -100, -100, -100, -100, -100, -100, -100 }, { -100, -100, -100, -100, -100, -100, -100, -100, -100, -100 }, { -100, -100, -100, -100, -100, -100, -100, -100, -100, -100 }, { -100, -100, -100, -100, -100, -100, -100, -100, -100, -100 }, { -100, -100, -100, -100, -100, -100, -100, -100, -100, -100 }, };
+        //  int[][] placa_nod = new int[10][] { { -100, -100, -100, -100, -100, -100, -100, -100, -100, -100 }, new int[10], new int[10], new int[10], new int[10], new int[10], new int[10], new int[10], new int[10], new int[10] };
+        int[] placa_RSSI = new int[10];
+
+        UInt16[] placas = new UInt16[10];
+        UInt16[] nodos = new UInt16[10];
+
+
+        System.IO.FileStream outputFS;
+        System.IO.StreamWriter outputWriter;
+
+        String LecturasRSSI;    //Nombre del archivo donde se guardan los datos.
+                                //C:\Users\eduardosc\Desktop\Prueba.txt
+
+        int contador_datos = 0;
+
+        bool started = false;
+        Thread readThread;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void start_button_Click(object sender, EventArgs e)
+        {
+            if (started)
+            {
+                start_button.Text = "START";
+                started = false;
+
+
+
+                try
+                {
+                    this.BeginInvoke((MethodInvoker)delegate         // runs on UI thread
+                    {
+                        trace_box.AppendText("***Finaliza el programa\r\n");
+                    });
+                }
+
+
+                catch (TimeoutException) { }
+
+            }
+            else
+            {
+                started = true;
+                start_button.Text = "STOP";
+                this.BeginInvoke((MethodInvoker)delegate         // runs on UI thread
+                {
+                    trace_box.AppendText("***Comienza el programa\r\n");
+                });
+            
+                readThread = new Thread(Read);
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.Close();
+                }
+                this.serialPort1.PortName = text_serial_port.Text;
+                serialPort1.Open();
+                readThread.Start();
+
+            }
+        }
+
+        public void Read()
+        {
+
+            try
+            {
+                byte[] data = new byte[1024];
+                while (true)
+                {
+                    //Read preambule
+                    int readed = serialPort1.Read(data, 0, 1);
+                    Byte preamb = data[0];
+                    while (readed == 0 || preamb != 0xAA)
+                    {
+                        readed = serialPort1.Read(data, 0, 1);
+                        preamb = data[0];
+
+                    }
+                    string measure;
+                    contador_datos++;
+
+                    readed = serialPort1.Read(data, 0, 10);
+                    UInt16 src_addr = BitConverter.ToUInt16(data, 0);
+                    UInt16 node_addr = BitConverter.ToUInt16(data, 2);
+                    int rssi = BitConverter.ToInt16(data, 3);
+
+                    if (data[4] == 45)
+                    {
+                        rssi = ((data[5] - 48) * 10 + (data[6] - 48)) * -1;
+                    }
+                    else
+                    {
+                        rssi = ((data[5] - 48) * 10 + (data[6] - 48));
+                    }
+
+                    this.BeginInvoke((MethodInvoker)delegate         // runs on UI thread
+                    {
+                        trace_box.AppendText("La placa " + src_addr + " ha sido detectado por el nodo " + node_addr + " con un RSSI de " + rssi + "dBm" + "\r\n");
+                    });
+
+                    LecturasRSSI = "Lecturas.txt";
+
+                    //a veces la interfaz se queda pillada con lo del outputWriter
+                    outputFS = (System.IO.FileStream)File.Open(LecturasRSSI, FileMode.Append, FileAccess.Write);
+
+                    outputWriter = new System.IO.StreamWriter(outputFS);
+                    outputWriter.NewLine = "\r\n";
+
+                    measure = src_addr + " " + node_addr + " " + rssi; 
+                    outputWriter.WriteLine(measure);
+
+                    outputWriter.Close();  //Cerramos el texto
+                    outputFS.Close();
+
+                    //HASTA AQUI BIEN
+
+
+                    //TENGO QUE GUARDAR EN UNA MATRIZ PLACA_NODO EL NODO QUE SE HA DETECTADO PARA CADA PLACA SINO SE PIERDE
+
+
+
+                    aux_nodo = node_addr;
+                    aux_placa = src_addr;
+                    for (int i = 0; i < nodos.Length - 1; i++)
+                    {
+                        if (nodos[i] == aux_nodo)
+                        {
+                            aux = 1;
+                        }
+                    }
+
+                    if (aux != 1)
+                    {
+                        nodos[posicion_nodo] = aux_nodo;
+                        posicion_nodo++;
+                    }
+
+                    for (int i = 0; i < placas.Length - 1; i++)
+                    {
+                        if (placas[i] == aux_placa)
+                        {
+                            aux2 = 1;
+                            if (placa_RSSI[i] <= rssi)
+                            {
+                                placa_RSSI[i] = rssi;
+                                var_node = node_addr;
+                                var_placa = src_addr;
+                                var_rssi = placa_RSSI[i];
+
+                            }
+                                                      
+                        }
+                    }
+                    if (aux2 != 1)
+                    {
+
+                        placas[posicion_placa] = aux_placa;
+                        placa_RSSI[posicion_placa] = rssi;
+                        posicion_placa++;
+                    }
+
+                  
+                    aux = 0;
+                    aux2 = 0;
+
+                    //HASTA AQUI BIEN
+
+                    this.BeginInvoke((MethodInvoker)delegate         // runs on UI thread
+                    {
+                      /*  for (int i = 0; i < placas.Length - 1; i++)
+                        {
+                            if (placas[i] != 0)
+                            {
+                                trace_box.AppendText("El RSSI más grande de la placa " + placas[i] + " detectado por el nodo " + var_node + " es de " + var_rssi +" dBm" + "\r\n");
+                            }
+                        }*/
+                                  
+                    });
+
+
+
+                }
+            }
+            catch (TimeoutException) { }
+
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            this.BeginInvoke((MethodInvoker)delegate         // runs on UI thread
+            {
+                trace_box.AppendText("Han pasado 20 segundos, reiniciamos valores\r\n");
+                posicion_nodo = 0;
+                posicion_placa = 0;
+                var_node = 0;
+                var_placa = 0;
+                var_rssi = -100;
+                for (int i = 0; i < nodos.Length - 1; i++)
+                {
+                    placas[i] = 0;
+                    nodos[i] = 0;
+                    placa_RSSI[i] = -100; 
+                }
+            });
+        }
+    }
+}
